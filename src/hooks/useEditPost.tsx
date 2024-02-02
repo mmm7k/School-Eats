1b7ab918da2fff5f006c074f02b4f8d226f71c4d
computerkingminsu <ya5656@naver.com>
2024-02-02 15:15:52 +0900

    refactor: directory

--- src/hooks/useEditPost.tsx
+++ src/hooks/useEditPost.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useState } from 'react';
 import { useMoveToPage } from './useMoveToPage';
 import { useRouter } from 'next/router';
-import { authInstance, db, firebaseapp } from '../../../pages/_app';
+import { authInstance, db, firebaseapp } from '../../pages/_app';
 import { addDoc, collection, doc, getFirestore, updateDoc } from 'firebase/firestore';
 
 export const useEditPost = () => {
