ac446bea922a35d5dfd2f34f14c6ea1b4f67ce3f
computerkingminsu <ya5656@naver.com>
2024-02-07 22:41:34 +0900

    fix: font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,6 +89,10 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         <meta property="og:description" content="대학생들의 점심 고민을 해결해줄 School Eats!"></meta>
         <link rel="icon" href="/logo.png"></link>
         <link rel="manifest" href="/manifest.json" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <RecoilRoot>
         <Global styles={globalStyles} />
