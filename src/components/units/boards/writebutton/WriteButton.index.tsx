f8da9d687587313b4d88627263f1aa395399496e
computerkingminsu <ya5656@naver.com>
2024-02-08 21:55:24 +0900

    fix: boardwrite loggedInerror

--- src/components/units/boards/writebutton/WriteButton.index.tsx
+++ src/components/units/boards/writebutton/WriteButton.index.tsx
@@ -2,9 +2,19 @@ import { useEffect, useState } from 'react';
 import * as S from './WriteButton.styled';
 import { EditOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import { useRecoilValue } from 'recoil';
+import { isLoggedIn } from '../../../../commons/globalstate/globalstate';
+import { Modal } from 'antd';
 
 export default function WriteButton(): JSX.Element {
   const [isScrolled, setIsScrolled] = useState(false);
+  const loggedIn = useRecoilValue(isLoggedIn);
+
+  const loginError = () => {
+    Modal.error({
+      title: '로그인이 필요합니다!',
+    });
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,11 +31,22 @@ export default function WriteButton(): JSX.Element {
   return (
     <>
       <S.Wrapper isScrolled={isScrolled}>
-        <Link href="/boards/write">
+        {/* <Link href="/boards/write">
           <S.Button>
             <EditOutlined style={{ color: 'white' }} rev={undefined} />
           </S.Button>
-        </Link>
+        </Link> */}
+        {loggedIn ? (
+          <Link href="/boards/write">
+            <S.Button>
+              <EditOutlined style={{ color: 'white' }} rev={undefined} />
+            </S.Button>
+          </Link>
+        ) : (
+          <S.Button onClick={loginError}>
+            <EditOutlined style={{ color: 'white' }} rev={undefined} />
+          </S.Button>
+        )}
       </S.Wrapper>
     </>
   );
